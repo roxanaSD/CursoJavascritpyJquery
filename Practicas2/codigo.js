ac806fba6963a4b68a7dd053d27b0a21bae7bf43
RoxanaaSD <roxana_sd26@hotmail.com>
2018-08-22 13:30:15 -0500

    Practicas JavaScript

---
+++ Practicas2/codigo.js
@@ -0,0 +1,7 @@
+alert("practica de inclusión!!"); //el alert genera un mensaje
+alert("Soy el primer script"); //genera un segundo mensaje
+
+function Saludar(nombre){ //realiza la funcion del mensaje
+document.write(`Hola ${nombre}`) //manda el mensaje dentro de la funcion
+}
+Saludar (" a todos... soy Roxana") //llama la funcion declarada y manda el mensaje
---
+++ Practicas2/documentoexterno.js
@@ -0,0 +1,19 @@
+alert("Función de suma:");
+function Suma(numer1, numer2){
+return numer1 + numer2}
+alert(Suma(40,50))
+
+alert("Función de Resta:")
+function resta(numer1, numer2){
+return numer1 - numer2}
+alert(resta(80,50))
+
+alert("Función de Multiplicación:")
+function multi(numer1, numer2){
+return numer1 * numer2}
+alert(multi(10,10))
+
+alert("Función de División:")
+function div(numer1, numer2){
+return numer1 / numer2}
+alert(resta(80,20))
